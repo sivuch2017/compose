be325322cce357cb323981b4ac75072b401396e8
sivuch2017 <say_each@yahoo.co.jp>
2019-04-26 19:51:41 +0900

    ldap mod

--- ldap/build/entrypoint.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env sh
-if [[ -n "$PHPLDAPADMIN_LDAP_HOST" ]]; then
-  sed -i "s/setValue('server','host','localhost')/setValue('server','host','`echo $PHPLDAPADMIN_LDAP_HOST | sed 's#\/#\\\/#g'`')/" /etc/phpldapadmin/config.php
-  sed -i "s/setValue('server','name','localhost')/setValue('server','name','`echo $PHPLDAPADMIN_LDAP_HOST | sed 's#\/#\\\/#g'`')/" /etc/phpldapadmin/config.php
-fi
-httpd -D FOREGROUND
